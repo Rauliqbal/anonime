d5954b7b50af2e9c4a0978c975779f2a34acf833
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2023-09-06 13:56:37 +0700

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Router from "./router";
 
@@ -5,9 +6,10 @@ function App() {
    return (
       <>
          <Navbar />
-         <main className="py-20 md:py-28">
+         <main className="py-20 md:py-24">
             <Router />
          </main>
+         <Footer/>
       </>
    );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+
+export default function Footer() {
+  return (
+    <div className='border-t border-slate-700 text-center text-white py-8'>
+      <span>Anonime made by <a className='border-b border-dashed border-blue-500' target="_blank" href="https://rauliqbal.my.id" rel="noreferrer">Rauliqbal ❤️</a></span>
+    </div>
+  )
+}
